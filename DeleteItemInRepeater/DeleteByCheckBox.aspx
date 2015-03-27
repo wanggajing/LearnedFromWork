f0a6b4b9aaca735347930eed800119d41c30dd0f
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-27 15:56:31 +1300

    Delete multiple items selected by checkbox

---
+++ DeleteItemInRepeater/DeleteByCheckBox.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DeleteByCheckBox.aspx.cs" Inherits="DeleteByCheckBox" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ProductName</th>
+                </tr>
+            </thead>
+            <tbody>
+                <asp:Repeater ID="Repeater1" runat="server" OnItemDataBound="Repeater1_ItemDataBound">
+                    <ItemTemplate>
+                        <tr>
+                            <td><%# Eval("Name") %></td>
+                            <td>
+                                <asp:CheckBox ID="CheckBox1" runat="server" value='<%# Eval("ProductId")%>' Visible="false"/>
+                            </td>
+                        </tr>
+                    </ItemTemplate>
+                </asp:Repeater>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" Visible="false" />
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ DeleteItemInRepeater/DeleteByCheckBox.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DeleteByCheckBox : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Button1.Visible = true;
+        if (!IsPostBack)
+        {
+            bindData();
+        }
+    }
+    private void bindData()
+    {
+        string s = System.Configuration.ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+        SqlConnection connection = new SqlConnection(s);
+        string sql = "select * from ProductTable";
+        SqlDataAdapter adapter = new SqlDataAdapter(sql, s);
+        DataTable table = new DataTable();
+        adapter.FillSchema(table, SchemaType.Source);
+        adapter.Fill(table);
+        Repeater1.DataSource = table;
+        Repeater1.DataBind();
+        table.Dispose();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string s = System.Configuration.ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+        SqlConnection connection = new SqlConnection(s);
+        connection.Open();
+        foreach (RepeaterItem item in Repeater1.Items)
+        {
+            CheckBox cbx = item.FindControl("CheckBox1") as CheckBox;
+            if (cbx != null)
+            {
+                if (cbx.Checked)
+                {
+                    int i = Convert.ToInt32(cbx.Attributes["value"]);
+                    string sql = "delete from ProductTable where ProductId="+i;
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        bindData();
+        connection.Close();
+    }
+    protected void Repeater1_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+        CheckBox cbx = e.Item.FindControl("CheckBox1") as CheckBox;
+        cbx.Visible = true;
+    }
+}
